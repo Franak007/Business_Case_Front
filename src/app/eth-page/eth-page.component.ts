6b570b336ae8109b1ddddeeb9e9a100d903548e3
Franak007 <franck.pertosa@gmail.com>
2023-09-15 12:30:31 +0200

    affichage tous les eth ok

--- src/app/eth-page/eth-page.component.ts
+++ src/app/eth-page/eth-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EthService} from "../eth.service";
+import {EthInterface} from "../eth-interface";
+
 
 @Component({
   selector: 'app-eth-page',
   templateUrl: './eth-page.component.html',
   styleUrls: ['./eth-page.component.css']
 })
-export class EthPageComponent {
+export class EthPageComponent implements OnInit{
+
+  constructor(private ethService: EthService) {}
+
+    ethList: EthInterface[] =[];
+
+  ngOnInit() {
+    this.ethService.getAllEth().subscribe(data=>{
+      this.ethList = data;
+    })
+  }
 
 }
